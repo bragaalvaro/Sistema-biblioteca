d3fcdcf7c56eb0155a4aa43208b6519d86bb7b69
bragaalvaro <alvaro.braga@aluno.educa.go.gov.br>
2026-09-21 18:01:12 +0000

    implementação objeto LIVRO e método ADICIONAR

---
+++ UniversoPaginas/Data/LivroRepository.cs
@@ -0,0 +1,12 @@
+namespace UmiversoPaginas.Data
+{
+    public class LivroRepository 
+    {
+       private static List<Livro> _tabelaLivros = new ListLivro ();
+       private static int proximoID = 1; // Variável auxiliar 
+       public void Adicionar (Livro livro) //método: adiciona livros na lista    }
+       (
+         livro.Id = proximoId++;
+         _tabelaLivros.Add(livro);
+       )
+}
---
+++ UniversoPaginas/Models/Livro.cs
@@ -0,0 +1,12 @@
+namespace UniversoPaginas.Models
+{
+    public class Livro
+    {
+        public int Id {get; set;}
+        public string Titulo {get; set;}
+        public string Autor {get; set;}
+        public bool Emprestado {get; set;}
+
+    }
+
+}
